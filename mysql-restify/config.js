d95d7ec7f8b72f31790fcedcccaf8034919c8341
Piscki F. Pratama <pisckipratama@gmail.com>
2020-04-06 21:52:19 +0700

    set folder to create another lab

---
+++ mysql-restify/config.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+module.exports = {
+  name: "mysql-restify",
+  hostname: "http://localhost",
+  version: "0.0.1",
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  db: {
+    get: mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "nopassword",
+      database: "test"
+    })
+  }
+}
