32894e571bef159f85e41a546a454d2df05c82b7
Piscki F. Pratama <pisckipratama@gmail.com>
2020-04-07 20:56:03 +0700

    fixing show data and add delete feature

--- tes-app/routes/index.js
+++ tes-app/routes/index.js
@@ -5,13 +5,30 @@ const models = require('../models/index');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   models.Siswa.findAll({}).then(result => {
-    console.log(result)
+    
     res.render('index', {
       title: 'Tess',
       user: req.session.user,
-      data: result[0].dataValues
+      data: JSON.stringify(result)
     });
   }).catch(err => res.send(err))
 });
+
+router.get('/list-siswa', (req, res) => {
+  models.Siswa.findAll({}).then(result => {
+    res.json(result)
+  }).catch(err => res.send(err))
+})
+
+router.get('/delete/:id', (req, res) => {
+  models.Siswa.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(() => {
+    console.log(`sukses delete data id ${req.params.id}`)
+    res.redirect('/home');
+  })
+})
 
 module.exports = router;
